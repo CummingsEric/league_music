3948152b2e9956e126fe90c42985490b57178ffa
CummingsEric <cummingseric02@gmail.com>
2022-03-19 21:56:08 -0400

    added methods to league_client.py

--- league_client.py
+++ league_client.py
@@ -1,23 +1,23 @@
-from urllib.parse import non_hierarchical
+import pandas as pd
 import requests
 import urllib3
-import json
 from leagueExceptions import *
 import os
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class leagueClient():
-    _player_stats = None
+    _allPlayers = None
     _game_info = None
     _summoner_name = None
-    _player_info = None
-    _myteam = None
-    _player_team = non_hierarchical
+    _activePlayer = None
+    _summoner_team = None
+    _events = None
+    _gameData = None
     
     def __init__(self):
         print("client started")
 
-    def LeagueClientRunning(self):
+    def leagueClientRunning(self):
         hostname = 'https://127.0.0.1:2999/liveclientdata/allgamedata' #example
         response = os.system("ping -c 1 " + hostname)
 
@@ -30,15 +30,19 @@ class leagueClient():
     def _getAllData(self):
         r = requests.get('https://127.0.0.1:2999/liveclientdata/allgamedata', verify=False)
         self._game_info = r.json()
+        self._activePlayer = self._game_info['activePlayer']
+        self._allPlayers = self._game_info['allPlayers']
+        self._events = pd.DataFrame.from_dict(self._game_info['events'])
+        self._gameData = self._game_info['gameData']
         #sett basic level information
-        self._set_player_stats()
+        self._setPlayerStats()
         
         return True
 
-    def _set_player_stats(self):
+    def _setPlayerStats(self):
         self._summoner_name = self._game_info['activePlayer']['summonerName']
-        self._player_info = next(filter(lambda players: players['summonerName'] == self._summoner_name, self._game_info['allPlayers']))
-        self._player_team = self._player_info['team']
+        self._summoner_data = next(filter(lambda players: players['summonerName'] == self._summoner_name, self._game_info['allPlayers']))
+        self._summoner_team = self._summoner_data['team']
 
     def _getPlayerStats(self):
         r = requests.get('https://127.0.0.1:2999/liveclientdata/playerlist', verify=False)
@@ -56,7 +60,7 @@ class leagueClient():
             print(e)
             raise LeagueClientError('unable to retrieve data from league client')
 
-    def get_all_data(self):
+    def getAllData(self):
         if(self._game_info):
             return(self._game_info)
         elif(self._getAllData()):
@@ -66,7 +70,7 @@ class leagueClient():
             #there is no data, and the league client is not running
             raise LeagueClientError('unable to retrieve data from league client')
 
-    def get_player_data(self):
+    def getPlayerData(self):
         if(self._game_info):
             return(self._player_stats)
         elif(self._getPlayerStats()):
@@ -76,35 +80,136 @@ class leagueClient():
             #there is no data, and the league client is not running
             raise LeagueClientError('unable to retrieve data from league client')
 
-    #functions for pulling data out of json information
-    def get_friendly_stats(self):
-        return list(filter(lambda players: players['team'] == self._player_team, self._game_info['allPlayers']))
+    #ALLPLAYER BASED METHODS
+    def getFriendlyStats(self):
+        return list(filter(lambda players: players['team'] == self._summoner_team, self._allPlayers))
 
-    def get_enemy_stats(self):
-        return list(filter(lambda players: players['team'] != self._player_team, self._game_info['allPlayers']))
+    def getEnemyStats(self):
+        return list(filter(lambda players: players['team'] != self._summoner_team, self._allPlayers))
+
+    def getFriendlySummonerNames(self):
+        return [x['summonerName'] for x in self.getFriendlyStats()]
+
+    def getEnemySummonerNames(self):
+        return [x['summonerName'] for x in self.getEnemyStats()]
+
+    def get_player_kda(self, summoner_name):
+        summoner_stats = list(filter(lambda players: players['summonerName'] == summoner_name, self._allPlayers))[0]
+        return (summoner_stats['scores']['assists'] + summoner_stats['scores']['kills'])/max(1, summoner_stats['scores']['deaths'])
+
+    def get_summoner_kda(self):
+        return self.get_player_kda(self._summoner_name)
+
+    def getFriendlyTeamKDA(self):
+        [(x['scores']['assists'] + x['scores']['kills'])/max(1, x['scores']['deaths']) for x in self.getFriendlyStats()]
+
+    def getEnemyTeamKDA(self):
+        [(x['scores']['assists'] + x['scores']['kills'])/max(1, x['scores']['deaths']) for x in self.getEnemyStats()]
+
+    def get_deathStreaks(self):
+        last_kill = self._events[self._events['EventName'] == 'ChampionKill'].merge((self._events[self._events['EventName'] == 'ChampionKill'].sort_values(['EventTime']).groupby('KillerName').agg({'EventTime':'max'})).reset_index(), left_on="VictimName", right_on="KillerName", how='left', suffixes=('_main', '_lastkill'))
+        last_kill['EventTime_lastkill'] = last_kill['EventTime_lastkill'].fillna(0)
+        current_deathstreak = last_kill[last_kill['EventTime_main'] > last_kill['EventTime_lastkill']].groupby('VictimName').agg({'EventTime_main':'count'})
+        current_deathstreak.reset_index(inplace=True)
+        current_deathstreak.columns = ['VictimName', 'Deaths']
+        return current_deathstreak[current_deathstreak['Deaths'] >= 5]
+
+    def get_killStreaks(self):
+        last_death = self._events[self._events['EventName'] == 'ChampionKill'].merge((self._events[self._events['EventName'] == 'ChampionKill'].sort_values(['EventTime']).groupby('VictimName').agg({'EventTime':'max'})).reset_index(), left_on="KillerName", right_on="VictimName", how='left', suffixes=('_main', '_lastdeath'))
+        last_death['EventTime_lastdeath'] = last_death['EventTime_lastdeath'].fillna(0)
+        current_killstreak = last_death[last_death['EventTime_main'] > last_death['EventTime_lastdeath']].groupby('KillerName').agg({'EventTime_main':'count'})
+        current_killstreak.reset_index(inplace=True)
+        current_killstreak.columns = ['KillerName', 'Kills']
+        return current_killstreak[current_killstreak['Kills'] >= 5]
+
+    def isEnemyOnKillStreak(self):
+        return (sum([1 if x in self.getEnemySummonerNames() else 0 for x in self.get_killStreaks()['KillerName']]) >=1)
+        
+
+    def isSummonerOnKillStreak(self):
+        return self._summoner_name in self.get_killStreaks()['KillerName']
+
+    def isSummonerOnDeathStreak(self):
+        return self._summoner_name in self.get_deathStreaks()['VictimName']
+
+    def getPlayerGold(self, summoner_name):
+        summoner_items = list(filter(lambda players: players['summonerName'] == summoner_name, self._allPlayers))[0]['items']
+        return sum([x['price']*x['count'] for x in summoner_items])
+        
+    def getSummonerGold(self):
+        return self.getPlayerGold(self._summoner_name)
+
+    def getFriendlyTeamGold(self):
+        return sum([self.getPlayerGold(x) for x in self.getFriendlySummonerNames()])
+
+    def getEnemyTeamGold(self):
+        return sum([self.getPlayerGold(x) for x in self.getEnemySummonerNames()])
 
 
+    #roles living or dead
+    #is only alive
+    def getFriendlyPlayerByPosition(self, position):
+        return list(filter(lambda players: players['position'] == '', self.getFriendlyStats()))[0]
 
-    def get_player_kda(self, stats):
-        killsAssists = stats['scores']['assists'] + stats['scores']['kills']
-        deaths = stats['scores']['deaths']
-        deaths = deaths if deaths != 0 else 1
-        return killsAssists / deaths
+    def getEnemyPlayerByPosition(self, position):
+        return list(filter(lambda players: players['position'] == '', self.getFriendlyStats()))[0]
 
-    def itemCost(self, stats):
-        allItems = stats['items']
-        cost = 0
-        for item in allItems:
-            cost += item['price']
-        return cost
+    def getPlayerPosition(self, summoner_name):
+        summoner_position = list(filter(lambda players: players['summonerName'] == summoner_name, self._allPlayers))[0]['position']
+        return summoner_position
 
-    def getDragons(self, summonerNames, events):
-        try:
-            dragonDeaths = list(filter(lambda event: event['EventName'] == 'DragonKill', events))
-            teamDragons = list(filter(lambda death: death['KillerName'] in summonerNames, dragonDeaths))
-            return len(teamDragons)
-        except:
-            return 0
+    def isPlayerDead(self, summoner_name):
+        return (list(filter(lambda players: players['summonerName'] == summoner_name, self._allPlayers))[0]['isDead'])
+        
+    def isFriendlyPositionDead(self, position):
+        return self.getFriendlyPlayerByPosition(position)['isDead']
+
+    def isAloneInBot(self):
+        summonerPosition = self.getPlayerPosition(self._summoner_name)
+        if(summonerPosition in ['BOTTOM', 'SUPPORT']):
+            return ((summonerPosition == 'SUPPORT' and self.isFriendlyPositionDead('BOTTOM')) or (summonerPosition == 'BOTTOM' and self.isFriendlyPositionDead('SUPPORT')))
+        else:
+            return False
+
+    def isAloneInGame(self):
+        friends = [x for x in self.getPlayerPosition(self._summoner_name) if x != self._summoner_name] #get friendly players with summoner removed
+        return ((sum([0 if self.isPlayerDead(x) else 1 for x in friends]) == 0) and not(self.isPlayerDead(self._summoner_name)))
+
+    #EVENT BASED METHODS
+
+    def getFriendlyTeamDragonKills(self):
+        return len(self._events[(self._events['EventName'] == 'DragonKill') & (self._events['KillerName'].isin(self.getFriendlySummonerNames()))])
+
+    def getEnemyTeamDragonKills(self):
+        return len(self._events[(self._events['EventName'] == 'DragonKill') & (self._events['KillerName'].isin(self.getEnemySummonerNames()))])
+
+    def getFriendlyTurrets(self):
+        team_to_name = {"T1":"CHAOS", "T2":"ORDER"}
+        turret_info = lc._events[lc._events['EventName'] == 'TurretKilled']['TurretKilled'].str.split('_',expand=True).drop([0,4], axis=1)
+        turret_info.columns = ['Team','Lane','Tier']
+        turret_info = turret_info.replace({"Team": team_to_name})
+        return turret_info[turret_info['Team']==self._summoner_team]
+
+    def getEnemyTurrets(self):
+        team_to_name = {"T1":"CHAOS", "T2":"ORDER"}
+        turret_info = lc._events[lc._events['EventName'] == 'TurretKilled']['TurretKilled'].str.split('_',expand=True).drop([0,4], axis=1)
+        turret_info.columns = ['Team','Lane','Tier']
+        turret_info = turret_info.replace({"Team": team_to_name})
+        return turret_info[turret_info['Team']!=self._summoner_team]
+
+    def getEnemyTurretsDestroyed(self):
+        return len(self.getEnemyTurrets())
+
+    def getFriendlyTurretsDestroyed(self):
+        return len(self.getFriendlyTurrets())
+
+    def friendlyNexusTurretsDown(self):
+        friendlyTurrets = self.getFriendlyTurrets()
+        return len(friendlyTurrets[(friendlyTurrets['Lane'] == 'C') & (friendlyTurrets['Tier'].isin(['01','02']))])
+
+    def enemyNexusTurretsDown(self):
+        enemyTurrets = self.getEnemyTurrets()
+        return len(enemyTurrets[(enemyTurrets['Lane'] == 'C') & (enemyTurrets['Tier'].isin(['01','02']))])
 
     def getTurrets(self, team, events):
         try:
@@ -114,10 +219,23 @@ class leagueClient():
         except:
             return 0
 
+    #GAMEDATA BASED METHODS
+    def getGametime(self):
+        return self._gameData['gameTime']
+
+    def getGameTerrain(self):
+        return self._gameData['mapTerrain']
+
 if __name__ == "__main__":
     # Switch the comments to get real data
     lc = leagueClient()
     lc.populateData()
     #print(lc.LeagueClientRunning())
-    print(lc._player_stats)
+    print(lc._summoner_data)
+    print("\n",len(lc.getFriendlyStats()))
+    for x in lc.getFriendlyStats():
+        print(x)
+    print("\n")
+    for x in lc.getEnemyStats():
+        print(x)
     #printData(json)
